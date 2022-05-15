aa02d083883a910bcbfafde5da562d53fcc44ff9
Lucas Fernandez <lucasfernandezaragon@gmail.com>
2022-05-15 19:10:32 +0200

    Fix edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,13 +24,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/lucferbux/Taller-Docusaurus/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/lucferbux/Taller-Docusaurus/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
